dadffca40ec35c3833e8026c7196877ca7759d40
Kalyani Joshi <Kalyani Joshi@DESKTOP-FUD903H>
2022-10-20 21:43:38 +0530

    hi

---
+++ src/javahub/gitdemo.java
@@ -0,0 +1,5 @@
+package javahub;
+
+public class gitdemo {
+
+}
---
+++ src/javahub/github.java
@@ -0,0 +1,11 @@
+package javahub;
+
+public class github {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+
+	}
+
+}
